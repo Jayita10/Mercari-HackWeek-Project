4b9f38ec98afd6a486ace5049603123b39a9b834
Jayita10 <jayitapramanik2018@gmail.com>
2022-05-26 21:59:49 +0530

    initial setup and sidebar component complete

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import App from "./App";
+
+// to hook our react application with root 
+ReactDOM.render(<App />, document.getElementById('root'));
