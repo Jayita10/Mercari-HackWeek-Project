11e8aab785393e40bc3dcbac5a430ce79d8f6f6a
Jayita10 <jayitapramanik2018@gmail.com>
2022-05-30 11:42:45 +0530

    working on footer, notification, chat, profile part

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
-import { Ecommerce, Orders, Customers, Calendar, Line, Pie, Editor, Employees, MainDashboard, ItemAnalysis, Listings, BuyerReview } from './pages';
+import { Ecommerce, Orders, Customers, Calendar, Line, Pie, Editor, Employees, MainDashboard, Listings, BuyerReview } from './pages';
 import { useStateContext } from './contexts/ContextProvider';
 import './App.css'
 
 const App = () => {
     const {setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings} = useStateContext();
+
+    // useEffect(() => {
+    //     const currentThemeColor = localStorage.getItem('colorMode');
+    //     const currentThemeMode = localStorage.getItem('themeMode');
+    //     if (currentThemeColor && currentThemeMode) {
+    //       setCurrentColor(currentThemeColor);
+    //       setCurrentMode(currentThemeMode);
+    //     }
+    // }, []);
 
   return (
     <div className={currentMode === 'Dark' ? 'dark' : ''}>
@@ -17,7 +26,7 @@ const App = () => {
             <div className='flex relative dark:bg-main-dark-bg'>
                 <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
                     <TooltipComponent content='Settings' position='Top'>
-                        <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' onClick={() => setThemeSettings(true)} style={{ background: currentColor, borderRadius: '50%' }}>
+                        <button type='button' className='text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' onClick={() => setThemeSettings(true)} style={{ background: currentColor, borderRadius: '50%' }}>
                             <FiSettings />
                         </button>
                     </TooltipComponent>
@@ -33,8 +42,9 @@ const App = () => {
                 )}
                 <div
                     className={
-                        `dark:bg-main-dark-bg  bg-main-bg min-h-screen w-full
-                         ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+                        activeMenu
+                        ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                        : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
                     }
                 >
                     <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
@@ -47,12 +57,14 @@ const App = () => {
                     <Routes>
                         {/* Dashboard */}
                         <Route path='/' element={<Ecommerce />} />
-                        <Route path='/mainDashboard' element={<Ecommerce />} />
+                        <Route path='/Dashboard' element={<Ecommerce />} />
                         <Route path='/listings' element={<Orders />} />
-                        <Route path='/itemAnalysis' element={<ItemAnalysis />} />
+                        <Route path='/itemAnalysis' element={<Line />} />
                         <Route path='/buyerReview' element={<BuyerReview />} />
+
                     </Routes>
                 </div>
+                <Footer />
                 </div>
             </div>
         </BrowserRouter>
